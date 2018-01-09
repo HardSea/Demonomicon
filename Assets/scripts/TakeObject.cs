2acfa9df5926d278d91f8743348d773e4c19a303
HardSea <androidmen2000@gmail.com>
2018-01-10 01:37:54 +0200

    first

---
+++ Assets/scripts/TakeObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeObject : MonoBehaviour {
+	public GameObject obj;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp ("Jump")) {
+			obj.transform.position = new Vector3 (544.51f, 206.45f, 17.43f);
+		}
+	}
+}
---
+++ Assets/scripts/controller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CnControls;
+
+public class controller : MonoBehaviour {
+	Vector3 position;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		position = new Vector3 (0f,CnInputManager.GetAxis ("Horizontal"),CnInputManager.GetAxis ("Vertical"));
+		transform.position += position * Time.deltaTime * 10;
+		Debug.Log (position);
+	}
+}
