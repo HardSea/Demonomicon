e67f1edda6290ba814f92ac781d8d3152eaf20b3
HardSea <androidmen2000@gmail.com>
2018-01-10 15:50:26 +0200

    Script

---
+++ Assets/pickupAxe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+
+public class pickupAxe : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject go;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+        Debug.Log("Clicked!");
+        Destroy(go);
+
+    }
+
+   
+}
+
